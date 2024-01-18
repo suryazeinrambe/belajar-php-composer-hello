e43481f18417ba211a007e6336de1befbddec929
Surya Zein Rambe <suryazein2002@gmail.com>
2024-01-18 22:44:17 +0700

    Contoh Library sederhana Composer

---
+++ Customer.php
@@ -0,0 +1,13 @@
+<?php 
+namespace suryazeinrambe\Belajar;
+
+class Customer {
+    public function __construct(private string $name)
+    {
+
+    }
+    public function sayHello(string $name):string 
+    {
+        return "Hello $name, My Name is $this->name";
+    }
+}
